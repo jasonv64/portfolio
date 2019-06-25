e5a7cb9589c61666519b7aa2815ec5457b596c2f
Jason Valadez <valadezj045@gmail.com>
2019-06-25 13:06:26 -0700

    about me section

--- public/src/navbar.js
+++ public/src/navbar.js
@@ -1,2 +1,8 @@
-
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $(".main-nav").css({ "opacity": "1" })
+        }
+    })
+});
 
